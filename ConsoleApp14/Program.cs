e2eab7ce4189389bda16d14c9948fbcc03f4ef9c
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-08-18 18:02:45 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите число 1");
+            //string num1 = Console.ReadLine();
+
+            //Console.WriteLine("Введите число 2");
+            //string num2 = Console.ReadLine();
+
+            //int n1 = Convert.ToInt32(num1);
+            //int n2 = Convert.ToInt32(num2);
+
+            //Console.WriteLine($"Сумма чисел: {n1 + n2}");
+            //№1
+            //Console.WriteLine("Введите первое число");
+            //double number1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число");
+            //double number2 = double.Parse(Console.ReadLine());
+
+            //double result = (number1 + number2) / 2;
+            //Console.WriteLine(result);
+
+            //№2
+            //Console.WriteLine("Введите первое число");
+
+            //string num1 = Console.ReadLine();
+            //int n1 = Convert.ToInt32(num1);
+
+            //Console.WriteLine("Введите второе число");
+
+            //string num2 = Console.ReadLine();
+            //int n2 = Convert.ToInt32(num2);
+
+            //Console.WriteLine("Введите третье число");
+
+            //string num3 = Console.ReadLine();
+            //int n3 = Convert.ToInt32(num1);
+
+            //int sum = n1 + n2 + n3;
+            //Console.WriteLine("Сумма чисел" + sum);           
+            //int proiz = n1 * n2 * n3;
+            //Console.WriteLine("Произведение чисел" + proiz);
+
+            //№3
+            //Console.WriteLine("Введите Рубли для конвертера валюты");
+            //string r = Console.ReadLine();
+            //decimal rub = Convert.ToInt32(r);
+
+            //Console.WriteLine("Сумма денежных средств в долларах США");
+            //decimal dollar = rub / 75;
+            //Console.WriteLine(dollar);
+
+            //Console.WriteLine("Сумма денежных средств в Евро");
+            //decimal euro = rub / 85;
+            //Console.WriteLine(euro);
+
+            //№4
+            //int a = int.Parse(Console.ReadLine());
+            //if(a % 2 == 0)
+            //{
+            //    Console.WriteLine(a);
+            //    Console.WriteLine("Число четное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число нечетное");
+            //}
+
+            //№5
+            //Console.WriteLine("Введите первое число");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Выберите операцию (*, -, +, /)");
+            //char znak = char.Parse(Console.ReadLine());
+
+            //switch (znak)
+            //{
+            //    case '*':
+            //        int result;
+            //        result = num1 * num2;
+            //        Console.WriteLine("Ваш ответ " + result);
+            //        break;
+            //    case '+':
+            //        result = num1 + num2;
+            //        Console.WriteLine("Ваш ответ " + result);
+            //        break;
+            //    case '-':
+            //        result = num1 - num2;
+            //        Console.WriteLine("Ваш ответ " + result);
+            //        break;
+            //    case '/':
+
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine(0);
+            //        }
+            //        else
+            //            Console.WriteLine("Ваш ответ " + num1 / num2);
+            //        break;
+            //}
+
+            //№6
+            //int chetnoe = 0;
+            //int nechetnoe = 0;
+            //int sumchetnoe = 0;
+            //int sumnechetnoe = 0;
+            //Console.WriteLine("Введите первое число диапазона");
+            //int value = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите последнее число диапазона");
+            //int limit = int.Parse(Console.ReadLine());
+
+            //while (value <= limit)
+            //{
+            //    if (value % 2 == 0)
+            //    {
+            //        chetnoe++;
+            //        sumchetnoe += value;
+
+            //    }
+            //    else
+            //    {
+            //        nechetnoe++;
+            //        sumnechetnoe += value;
+
+            //    }
+            //    value++;
+            //}
+
+            //Console.WriteLine("Количество четных чисел: " + chetnoe);
+            //Console.WriteLine("Количество нечетных чисел: " + nechetnoe);
+
+            //Console.WriteLine("Сумма четных чисел: " + sumchetnoe);
+            //Console.WriteLine("Сумма нечетных чисел: " + sumnechetnoe);
+
+            //int i = int.Parse(Console.ReadLine());
+            //for(; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0,j = 5; i < 5; i++, j--)
+            //{
+            //    Console.WriteLine("i: " + i);
+            //    Console.WriteLine("j: " + j);
+            //}
+
+            //for(int i = 1; i <=3; i++)
+            //{
+            //    Console.WriteLine("цикл 1 итерация №: " + i);
+            //    for(int j = 1; j <=3; j++)
+            //    {
+            //        Console.WriteLine("\tцикл 2 итерация №: " + j);
+            //    }
+
+            //}
+            //Console.Write("Ведите высоту прямоугольника: ");
+            //int height = int.Parse(Console.ReadLine());
+            //for(int i = 0; i < height; i++)
+            //{
+            //    for(int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for(int i = height - 1; i >= 0; i--)
+            //{
+            //    for (int j = i - 1; j >= 0; j--)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+    }
+}
